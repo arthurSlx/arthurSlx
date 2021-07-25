436b6bd4f70182bbf929f05df70878cb354c7df4
Arthur Chen <arthur@sellerlinx.com>
2021-07-25 14:14:48 +0800

    modified routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: environment.production ? '**' : '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
